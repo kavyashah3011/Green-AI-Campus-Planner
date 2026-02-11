3417ea3bef84024b248253c6fe15b8f0eaf10179
KAVYA SHAH <116359861+kavyashah3011@users.noreply.github.com>
2026-02-11 21:56:14 +0530

    Delete backend directory

--- backend/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-
-    recs.append("Increase tree plantation in identified green clusters")
-    return jsonify(recs)
-
-# ---------------- RUN ----------------
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
